6c806da33e13a1b1291f894e3fe1b9a7e5ba77fa
Rodolfo Belo <64740047+RodolfoBelo19@users.noreply.github.com>
2023-02-20 13:25:06 -0300

    first commit

---
+++ src/domain/models/detected-image.ts
@@ -0,0 +1,5 @@
+export interface DetectedImage {
+  description: string;
+  timestamp: Date;
+  label: string;
+}
---
+++ src/presentation/pages/PhotoUpload/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useCallback } from "react";
+import { SendImage } from "../../../domain/usecases/send-image";
+
+type Props = {
+  sendImage: SendImage;
+};
+
+export const PhotoUpload: React.FC<Props> = ({ sendImage }) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
+  const [description, setDescription] = useState<string>("");
+
+  const handleFileChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      setFile(file);
+      setFileUrl(URL.createObjectURL(file));
+    }
+  }, []);
+
+  const handleDescriptionChange = useCallback((event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (file) {
+      await sendImage({ file, description });
+    }
+  }, [file, description]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileChange} />
+      <input type="text" value={description} onChange={handleDescriptionChange} />
+      <button type="submit">Enviar</button>
+    </form>
+  )
+
+};
